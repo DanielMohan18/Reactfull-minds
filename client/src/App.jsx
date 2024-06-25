6bdd7b0d656d478286ce2afed158a972628d6085
DanielMohan18 <154761244+DanielMohan18@users.noreply.github.com>
2024-06-26 01:54:21 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-export default function App() {
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Projects from './pages/Projects'
+import Signup from './pages/Signup'
+// import Header from './components/Header'
+const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+    {/* <Header /> */}
+    <Routes>
+     <Route path='/' element={<Home />}/>
+     <Route path='/dashboard' element={<Dashboard />}/>
+     <Route path='/about' element={<About />}/>
+     <Route path='/signin' element={<Signin />}/>
+     <Route path='signup' element={<Signup />}/>
+     <Route path='/projects' element={<Projects />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
+export default App
